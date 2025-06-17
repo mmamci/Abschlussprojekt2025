d4040b9150d825664b16aaa674b8ee817723a54d
mmamci <mm3505@mci4me.at>
2025-06-17 14:51:34 +0200

    created ui with mutliple pages and started creating "add variable" functionality

--- pages/variable_hinzufuegen.py
+++ pages/variable_hinzufuegen.py
@@ -5,5 +5,36 @@ st.title("Variable Hinzufügen","variable_hinzufuegen")
 
 st.text_input("Name der Variable")
 st.text_input("Einheit")
-st.text_input("Benachrichtigung")
+
+
+
+@st.dialog("Create Notification")
+def create_notificaiton():
+    st.write("Benachrichtigung erstellen")
+    notification_types = ["Daily", "Weekly", "Monthly"]
+    
+    notification_type = st.selectbox("Benachrichtigungstyp", notification_types)
+    
+    notification_times = []
+
+    if notification_type == "Weekly":
+        weekdays = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
+        st.pills("Wochentage", options=weekdays, selection_mode="multi")
+
+    elif notification_type == "Daily":
+        times_a_day = st.selectbox("Wie oft pro Tag?", list(range(1, 11)))
+
+        for i in range(1, times_a_day+1):
+            notification_label = f"{str(i)}\. Erinnerungszeitpunkt"
+            notification_times.append(st.time_input(notification_label))
+
+col1, col2 = st.columns([4, 1])
+with col1:
+    st.write("Keine Benachrichtigungen")
+with col2:
+    if st.button("Erstellen"):
+        create_notificaiton()
+
 st.text_input("Ziel der festgelegten variable")
+
+st.button("Variable Erstellen")
