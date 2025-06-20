43c98cf1058bd4114f4da7fd26fb092ce1729149
mmamci <mm3505@mci4me.at>
2025-06-20 21:08:01 +0200

    started writing add variable functionality, doesn't work yet

--- pages/variable_hinzufuegen.py
+++ pages/variable_hinzufuegen.py
@@ -1,39 +1,49 @@
 import streamlit as st
 
-
 st.title("Variable Hinzufügen","variable_hinzufuegen")
 
 st.text_input("Name der Variable")
 st.text_input("Einheit")
 
-
+if "notification_times" not in st.session_state:
+    st.session_state.notification_times = []
+if "notification_type" not in st.session_state:
+    st.session_state.notification_type = ""
+    
+ss = st.session_state
 
 @st.dialog("Create Notification")
 def create_notificaiton():
     st.write("Benachrichtigung erstellen")
     notification_types = ["Daily", "Weekly", "Monthly"]
     
-    notification_type = st.selectbox("Benachrichtigungstyp", notification_types)
+    ss.notification_type = st.selectbox("Benachrichtigungstyp", notification_types)
     
-    notification_times = []
 
-    if notification_type == "Weekly":
+    if ss.notification_type == "Weekly":
         weekdays = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
         st.pills("Wochentage", options=weekdays, selection_mode="multi")
 
-    elif notification_type == "Daily":
+    elif ss.notification_type == "Daily":
         times_a_day = st.selectbox("Wie oft pro Tag?", list(range(1, 11)))
 
         for i in range(1, times_a_day+1):
-            notification_label = f"{str(i)}\. Erinnerungszeitpunkt"
-            notification_times.append(st.time_input(notification_label))
+            notification_label = f"{str(i)}\\. Erinnerungszeitpunkt"
+            ss.notification_times.insert(i-1, st.time_input(notification_label))
 
-col1, col2 = st.columns([4, 1])
-with col1:
-    st.write("Keine Benachrichtigungen")
-with col2:
-    if st.button("Erstellen"):
-        create_notificaiton()
+        if st.button("Fertig"):
+            st.rerun()
+        
+
+# st.write("Benachrichtigungen")
+
+
+if len(ss.notification_times) == 0:
+    st.pills("Benachrichtigungen", ["\+"], on_change=create_notificaiton)
+
+elif ss.notification_type == "Daily":
+        notification_times_str = [i.strftime("%H:%M") for i in ss.notification_times]
+        st.pills("Benachrichtigungen", notification_times_str)
 
 st.text_input("Ziel der festgelegten variable")
 
