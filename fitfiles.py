3e1e575ac4795c0108af2aa9bf252bef3d205d97
SN0204 <no5933@mci4me.at>
2025-06-21 12:15:15 +0200

    Fitfiles einlesen

---
+++ fitfiles.py
@@ -0,0 +1,18 @@
+from fitparse import FitFile 
+
+def read_fit_file(file_path):
+    """
+    Liest ein FIT-File und gibt die Daten als Liste von Dictionaries zurück.
+    """
+    fitfile = FitFile(file_path)
+    data = []
+
+    for record in fitfile.get_messages('record'):
+        record_data = {}
+        for field in record:
+            record_data[field.name] = field.value
+        data.append(record_data)
+
+    return data 
+
+
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 
 import streamlit as st
-from streamlit_calendar import calendar
-from kalender import get_calendar_events
 
 st.set_page_config(
     page_title="Kalender",
@@ -9,22 +7,3 @@ st.set_page_config(
     page_icon="📅",
     initial_sidebar_state="expanded"
 )
-
-st.title("📆 Kalender Ansicht")
-
-events = get_calendar_events()
-
-calendar_options = {}
-
-
-calendar(events=events, options=calendar_options)
-
-pages = st.navigation(
-    [
-        st.Page("pages/ansicht.py", title="Anischt", icon=":material/monitoring:"),
-        st.Page("pages/highlights.py", title="Highlights"),
-        st.Page("pages/variable_hinzufuegen.py", title="Variable Hinzufügen"),
-    ]
-)
-pages.run()
-
