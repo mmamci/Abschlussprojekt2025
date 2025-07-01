101fead6d358f13a9bcec2f9fe8ea3a193774fde
mmamci <mm3505@mci4me.at>
2025-07-01 22:58:01 +0200

    deleted main because streamlit wont let me hide it

--- main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from utils.authenticator import Authenticator
-from utils.variable import VariableHandle
-
-ss = st.session_state
-if "variableHandle" not in ss:
-    ss.variableHandle = VariableHandle()
-
-authenticator = Authenticator()
